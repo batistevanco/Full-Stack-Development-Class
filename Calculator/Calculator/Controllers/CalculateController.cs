9edee6a11ea822ac40ffaa942460f59c11fc7a8d
Batiste Vancoillie <batiste.vancoillie@student.vives.be>
2025-10-02 11:50:11 +0200

    calculator les 2

---
+++ Calculator/Calculator/Controllers/CalculateController.cs
@@ -0,0 +1,19 @@
+﻿using Calculator.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalcVM calcVM)
+        {
+            return View(calcVM);
+        }
+    }
+}
---
+++ Calculator/Calculator/ViewModels/CalcVM.cs
@@ -0,0 +1,13 @@
+﻿using Calculator.Controllers;
+
+namespace Calculator.ViewModels
+{
+    public class CalcVM
+    {
+        public int Getal1 { get; set; }
+        public int Getal2 { get; set; }
+
+
+
+    }
+}

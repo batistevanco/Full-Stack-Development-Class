d939ade840d54a7fab7fc69bd864acb611c26fc8
Batiste Vancoillie <batiste.vancoillie@student.vives.be>
2025-10-09 08:32:13 +0200

    MusicStore oefening

--- Calculator/Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edee6a11ea822ac40ffaa942460f59c11fc7a8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8acda081b41aa39b0c5df622a0bad5cfb597db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
